d4eb1d78f0f39b60e85fa4aebdf9249ce0b65d57
Mateusz Bardel <bardel@student.agh.edu.pl>
2023-03-08 20:43:35 +0000

    File rename

---
+++ proj1/RandomGraphs.py
@@ -0,0 +1,58 @@
+from DataTypes import *
+import random
+
+
+def generateCompleteEdgeList(size: int) -> list[tuple[int, int]]:
+    """Generates list of tuples representing all possible edges of a simple graph.
+    No loops are generated.
+
+    Args:
+        size (int): Number of vertices.
+
+    Returns:
+        list[tuple[int, int]]: List of possible edges.
+    """
+    return list(
+        [(x, y) for x in range(size) for y in range(size) if x < y]
+    )  # I'm sorry.
+
+
+def randomGraphByEdges(size: int, edges: int) -> GraphData:
+    """Generates random undirected graph with specific number of edges.
+
+    Args:
+        size (int): Number of vertices.
+        edges (int): Number of edges.
+
+    Returns:
+        GraphData: Object in AdjacencyList format.
+    """
+    data = [[] for _ in range(size)]  # Initialize empty Adjancency List
+
+    for n1, n2 in sorted(random.sample(generateCompleteEdgeList(size), edges)):
+        data[n1].append(n2)
+        data[n2].append(n1)
+
+    return AdjacencyList(size, data, edges)
+
+
+def randomGraphByChance(size: int, probability: float) -> GraphData:
+    """Generates random undirected graph with specified probability for any edge to appear.
+
+    Args:
+        size (int): Number of vertices.
+        probability (float): Probability for an edge to be created.
+
+    Returns:
+        GraphData: Object in AdjacencyList format.
+    """
+    data = [[] for _ in range(size)]  # Initialize empty Adjancency List
+    edges = 0
+
+    for n1, n2 in generateCompleteEdgeList(size):
+        if random.random() <= probability:
+            edges += 1
+            data[n1].append(n2)
+            data[n2].append(n1)
+
+    return AdjacencyList(size, data, edges)
