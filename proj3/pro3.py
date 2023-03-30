35528365baf6f81dd57316c26dc031e201543378
Aiden <marcel11235813@gmail.com>
2023-03-30 22:16:45 +0200

    fix minor bugs

--- proj3/pro3.py
+++ proj3/pro3.py
@@ -2,6 +2,7 @@ import proj1.DataTypes
 import proj1.RandomGraphs as rg
 import random
 
+# TODO: SPRAWDZ CZY GRAF JEST SPOJNY
 def random_graph_weights(size: int, edges:int, min_weight : int =1, max_weight : int =10 ):
     
     # step 1: generate random graph without weights
@@ -9,22 +10,23 @@ def random_graph_weights(size: int, edges:int, min_weight : int =1, max_weight :
     
     # step 2: assign random weights to edges
     for i in range(len(graph.data)):
-        for j, val in enumerate(range(len(graph.data[i]))):
-            if j < i:
-                if val != 0:
-                    curr = random.randint(min_weight,max_weight)
-                    graph.data[i][j] = curr
-                    graph.data[j][i] = curr
-                else:
-                    graph.data[i][j] = None
-                    graph.data[j][i] = None                   
+        for j, val in enumerate(graph.data[i]):
+            if j > i:
+                continue
+            if val != 0:
+                curr = random.randint(min_weight,max_weight)
+                graph.data[i][j] = curr
+                graph.data[j][i] = curr
+            else:
+                graph.data[i][j] = None
+                graph.data[j][i] = None                   
                 
     return graph
     
 def dijkstra(graph, start: int = 0):
+
     d = {}
     p = {}
-
 
     # initialisation
     for vertex in range(graph.size):
@@ -41,9 +43,8 @@ def dijkstra(graph, start: int = 0):
         u = sorted([vertex for vertex in range(graph.size) if vertex not in ready], key=d.get)[0]
         ready.add(u)
         
-        #todo: fix it to None
-        # realx
-        for v in [vertex for vertex in range(graph.size) if (vertex not in ready and graph.data[u][vertex] != 0)]:
+        # realax
+        for v in [vertex for vertex in range(graph.size) if (vertex not in ready and graph.data[u][vertex] is not None)]:
             if d[v] > d[u] + graph.data[u][v]:
                 d[v] = d[u] + graph.data[u][v]
                 p[v] = u
--- proj3/z2.py
+++ proj3/z2.py
@@ -2,7 +2,7 @@ import proj3.pro3 as p3
 
 # 1
 
-graph = p3.random_graph_weights(4,2)
+graph = p3.random_graph_weights(4,5)
 print(graph)
 
 # 2
