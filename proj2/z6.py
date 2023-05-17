69c85f9aabee56fe57b9cfc1a5729d6673f5e573
Aiden <80407697+marLoster@users.noreply.github.com>
2023-05-17 16:24:02 +0200

    Update z6.py

--- proj2/z6.py
+++ proj2/z6.py
@@ -27,19 +27,35 @@ def components(graph):
             all_comps.append(comp)
     return max(all_comps, key=len)
 
+def walk(graph,cycle,current_node):
+    if len(cycle) == len(graph[0]) and graph[current_node][cycle[0]]:
+        return cycle
+    
+    for node,exists in enumerate(graph[current_node]):
+        if exists and node not in cycle:
+            cycle_copy = cycle.copy()
+            cycle_copy.append(node)
+            walk_res =  walk(graph,cycle_copy,node)
+            if walk_res:
+                return walk_res
+            
+    return False
 
 def isHamilton(graph):
     comp = components(graph)
-    print(comp)
     if len(comp) == graph.__len__():
-        return True
+        start_node = 0
+        cycle = [0]
+        return walk(nx.to_numpy_array(graph),cycle,start_node)
     else:
         return False
 
 
 if __name__ == "__main__":
-    graph = Global.GraphicSequence.graphFromSequence([6,6,6,4,4,2,2,2]).to_networkx()
+    graph = Global.GraphicSequence.graphFromSequence([2,2,2,2,2,2]).to_networkx()
+    for _ in range(10):
+        graph = rg.randomize_graph(graph)
     draw_graph(graph)
-    plt.savefig("z6.png")
+    plt.savefig("z6.png") #save randomly generated graph
     plt.clf()
     print(isHamilton(graph))
